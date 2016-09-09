edc9655a863777b832749a322759ad4a7ea6f3dd
llxff <ll.wg.bin@gmail.com>
2016-09-09 19:09:27 +0300

    channels

--- web/static/js/actions/sessions.js
+++ web/static/js/actions/sessions.js
@@ -8,6 +8,22 @@ function setCurrentUser(dispatch, user) {
     type: Constants.CURRENT_USER,
     currentUser: user
   });
+
+  const socket = new Socket('/socket', {
+    params: { token: localStorage.getItem('phoenixAuthToken') }
+  });
+
+  socket.connect();
+
+  const channel = socket.channel(`users:${ user.id }`);
+
+  channel.join().receive('ok', () => {
+    dispatch({
+      type: Constants.SOCKET_CONNECTED,
+      socket: socket,
+      channel: channel
+    })
+  });
 }
 
 const Actions = {
--- web/static/js/reducers/session.js
+++ web/static/js/reducers/session.js
@@ -2,6 +2,8 @@ import Constants from '../constants';
 
 const initialState = {
   currentUser: null,
+  socket: null,
+  channel: null,
   error: null
 };
 
@@ -15,6 +17,9 @@ export default function reducer(state = initialState, action = {}) {
 
     case Constants.USER_SIGNED_OUT:
       return initialState;
+
+    case Constants.SOCKET_CONNECTED:
+      return { ...state, socket: action.socket, channel: action.channel };
 
     default:
       return state;
