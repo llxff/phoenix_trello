5150c690e578e4e120ac8cb3851623aedcbeadba
Александр Фомин <ll.wg.bin@gmail.com>
2016-09-15 11:48:42 +0300

    use monitor in channel

--- web/channels/board_channel.ex
+++ web/channels/board_channel.ex
@@ -1,11 +1,24 @@
 defmodule PhoenixTrello.BoardChannel do
   use PhoenixTrello.Web, :channel
-  alias PhoenixTrello.Board
+
+  alias PhoenixTrello.{User, Board, UserBoard, List, Card, Comment, CardMember}
+  alias PhoenixTrello.BoardChannel.Monitor
 
   def join("boards:" <> board_id, _params, socket) do
+    current_user = socket.assigns.current_user
     board = get_current_board(socket, board_id)
 
+    connected_users = Monitor.user_joined(board_id, current_user.id)
+
+    send(self, {:after_join, connected_users})
+
     { :ok, %{ board: board }, assign(socket, :board, board) }
+  end
+
+  def handle_info({:after_join, connected_users}, socket) do
+    broadcast! socket, "user:joined", %{users: connected_users}
+
+    {:noreply, socket}
   end
 
   def handle_in("members:add", %{"email" => emal}, socket) do
