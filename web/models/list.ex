030755f30724ae9ce9fd07feedd6ef6918e302b6
Александр Фомин <ll.wg.bin@gmail.com>
2016-09-26 11:00:44 +0300

    list model

--- web/models/list.ex
+++ web/models/list.ex
@@ -1,6 +1,10 @@
 defmodule PhoenixTrello.List do
   use PhoenixTrello.Web, :model
 
+  alias PhoenixTrello.{Board, List}
+
+  @derive {Poison.Encoder, only: [:id, :board_id, :name]}
+
   schema "lists" do
     field :name, :string
     belongs_to :board, PhoenixTrello.Board
