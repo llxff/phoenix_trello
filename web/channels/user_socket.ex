95d02a1f0f59e607e9f7640d19b1d4a56472e8e3
Александр Фомин <ll.wg.bin@gmail.com>
2016-10-23 16:52:15 +0300

    fix module

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -1,7 +1,7 @@
 defmodule PhoenixTrello.UserSocket do
   use Phoenix.Socket
 
-  alias PhoenixTrello.{Repo, User}
+  alias PhoenixTrello.{GuardianSerializer}
 
   channel "users:*", PhoenixTrello.UserChannel
   channel "boards:*", PhoenixTrello.BoardChannel
@@ -12,7 +12,7 @@ defmodule PhoenixTrello.UserSocket do
   def connect(%{"token" => token}, socket) do
     case Guardian.decode_and_verify(token) do
       { :ok, claims } ->
-        case Guardian.Serializer.from_token(claims["sub"]) do
+        case GuardianSerializer.from_token(claims["sub"]) do
           { :ok, user } ->
             { :ok, assign(socket, :current_user, user) }
           { :error, _reason } ->
