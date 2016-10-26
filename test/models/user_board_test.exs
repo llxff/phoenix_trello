5ec4d6d90b99196700ed00b8e5634bf6b766157a
Александр Фомин <ll.wg.bin@gmail.com>
2016-10-26 17:03:33 +0300

    fix user_board_test

--- test/models/user_board_test.exs
+++ test/models/user_board_test.exs
@@ -3,7 +3,7 @@ defmodule PhoenixTrello.UserBoardTest do
 
   alias PhoenixTrello.UserBoard
 
-  @valid_attrs %{}
+  @valid_attrs %{user_id: 1, board_id: 2}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
